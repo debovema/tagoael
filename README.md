76c2e9f8b21efb8da9437b496d081c03418728d7
Mathieu Debove <debovemathieu@gmail.com>
2016-06-03 16:09:59 +0200

    Adding a switch flag to change override behaviour between command line and configuration file

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The goal is to understand how to use Flæg &amp; Stært. It's written in Go and 
 
 Display automatically formatted help message with `-h` short flag:
 ```shell
-$ ./tagoael.exe -h
+$ ./tagoael -h
 tagoæl is an enhanced Hello World program to display messages with
 an advanced configuration mechanism provided by flæg & stært.
 
@@ -23,21 +23,23 @@ Usage: tagoael [--flag=flag_argument] [-f[flag_argument]] ...     set flag_argum
    or: tagoael [--flag[=true|false| ]] [-f[true|false| ]] ...     set true/false to boolean flag(s)
 
 Flags:
+        -c, --commandlineoverridesconfigfile               Whether configuration from command line overrides configuration from configuration file or not. (default "true")
         --configfile                                       Configuration file to use (TOML). (default "tagoael")
         -i, --displayindex                                 Whether to display index of each message (default "false")
-        -m, --messagetodisplay                             Message to display (default "Hello World")
-        -n, --numbertodisplay                              Number of message to display (default "5")
+        -m, --messagetodisplay                             Message to display (default "HELLO WOLRD")
+        -n, --numbertodisplay                              Number of messages to display (default "1000")
         -h, --help                                         Print Help (this message) and exit
 ```
 
 Revert to the simplest Hello World:
 ```shell
-$ ./tagoael.exe --messageToDisplay='Hello world' --displayIndex=false --numberToDisplay=1
+$ ./tagoael --messageToDisplay='Hello world' --displayIndex=false --numberToDisplay=1
+Hello World
 ```
 
 It is possible to combine short flags "-in10" = "--displayindex  --numbertodisplay=10":
 ```shell
-$ ./tagoael.exe -in10 --messageToDisplay='Gooooooo!'
+$ ./tagoael -in10 --messageToDisplay='Gooooooo!'
 1: Gooooooo!
 2: Gooooooo!
 3: Gooooooo!
@@ -50,9 +52,39 @@ $ ./tagoael.exe -in10 --messageToDisplay='Gooooooo!'
 10: Gooooooo!
 ```
 
-Last it is possible to provide configuration values from a TOML file
+Last it is possible to provide configuration values from a TOML file:
 ```shell
-$ ./tagoael.exe --configFile=tagoael.toml.sample
+$ ./tagoael --configFile=tagoael.toml.sample
+1: Hello from TOML
+2: Hello from TOML
+3: Hello from TOML
+4: Hello from TOML
+5: Hello from TOML
+6: Hello from TOML
+7: Hello from TOML
+8: Hello from TOML
+9: Hello from TOML
+10: Hello from TOML
+```
+
+By default, configuration from command line will override the one from file:
+```shell
+$ ./tagoael --configFile=tagoael.toml.sample --displayIndex=false
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+Hello from TOML
+```
+
+unless you define a flag (commandLineOverridesConfigFile) to change this behaviour ("--displayIndex=false" but is true in the TOML file):
+```shell
+$ ./tagoael --configFile=tagoael.toml.sample --displayIndex=false --commandLineOverridesConfigFile=false
 1: Hello from TOML
 2: Hello from TOML
 3: Hello from TOML
--- tagoael.go
+++ tagoael.go
@@ -1,38 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "github.com/containous/flaeg"
-    "github.com/containous/staert"
-    "os"
+	"fmt"
+	"github.com/containous/flaeg"
+	"github.com/containous/staert"
+	"os"
 )
 
 // all command line flags
 type GlobalConfiguration struct {
-	MessageToDisplay string	`short:"m" description:"Message to display"`
-	NumberToDisplay	 int	`short:"n" description:"Number of message to display"`
-	DisplayIndex	 bool	`short:"i" description:"Whether to display index of each message"`
+	MessageToDisplay string `short:"m" description:"Message to display"`
+	NumberToDisplay  int    `short:"n" description:"Number of message to display"`
+	DisplayIndex     bool   `short:"i" description:"Whether to display index of each message"`
 }
 
 // the configuration is composed of all command line flags + flag to use a TOML file
 type TagoaelConfiguration struct {
 	GlobalConfiguration
-	ConfigFile string `description:"Configuration file to use (TOML)."`
+	ConfigFile						string `description:"Configuration file to use (TOML)."`
+	CommandLineOverridesConfigFile	bool `short:"c" description:"Whether configuration from command line overrides configuration from configuration file or not."`
 }
 
 func DefaultTagoaelConfiguration() *TagoaelConfiguration {
 	return &TagoaelConfiguration{
 		GlobalConfiguration: GlobalConfiguration{
-			MessageToDisplay:	"HELLO WORLD",
-			NumberToDisplay:	1000,
-			DisplayIndex:		true,
+			MessageToDisplay: "HELLO WORLD",
+			NumberToDisplay:  1000,
+			DisplayIndex:     true,
 		},
-		ConfigFile: "tagoael", // by default looks
+		ConfigFile: "tagoael", // by default it looks for a 'tagoael.toml' file (in the current directory)
+		CommandLineOverridesConfigFile: true,
 	}
 }
 
 func DefaultTagoaelPointersConfiguration() *TagoaelConfiguration {
-		return &TagoaelConfiguration{}
+	return &TagoaelConfiguration{}
 }
 
 func main() {
@@ -40,35 +42,41 @@ func main() {
 	defaultPointersConfiguration := DefaultTagoaelPointersConfiguration()
 
 	// the main commad line (just "./tagoael")
-    mainCommand := &flaeg.Command{
-        Name: "tagoael",
-        Description: "tagoæl is an enhanced Hello World program to display messages with\n"+
-					 "an advanced configuration mechanism provided by flæg & stært.\n\n"+
-					 "flæg:   https://github.com/containous/flaeg\n"+
-					 "stært:  https://github.com/containous/staert\n"+
+	mainCommand := &flaeg.Command{
+		Name: "tagoael",
+		Description: "tagoæl is an enhanced Hello World program to display messages with\n" +
+					 "an advanced configuration mechanism provided by flæg & stært.\n\n" +
+					 "flæg:   https://github.com/containous/flaeg\n" +
+					 "stært:  https://github.com/containous/staert\n" +
 					 "tagoæl: https://github.com/debovema/tagoael\n",
-        Config: defaultConfiguration,
-        DefaultPointersConfig: defaultPointersConfiguration,
- 		Run: func() error {
+		Config:                defaultConfiguration,
+		DefaultPointersConfig: defaultPointersConfiguration,
+		Run: func() error {
 			run(defaultConfiguration)
 			return nil
 		},
-    }
+	}
 
 	// use flaeg to translate command line arguments to configuration
-    f := flaeg.New(mainCommand, os.Args[1:])
+	f := flaeg.New(mainCommand, os.Args[1:])
 	if _, err := f.Parse(mainCommand); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
 
-	// use staert to optionally override configuration from a TOML configuration file 
+	// use staert to optionally override configuration from a TOML configuration file
 	s := staert.NewStaert(mainCommand)
 	toml := staert.NewTomlSource("tagoael", []string{defaultConfiguration.ConfigFile, "."})
 
-	// add sources to staert 
-	s.AddSource(toml) // include the TOML file
-	s.AddSource(f) // include flaeg
+	// add sources to staert
+	if defaultConfiguration.CommandLineOverridesConfigFile {
+		s.AddSource(toml) // include the TOML file
+		s.AddSource(f)    // include flaeg
+	} else {
+		s.AddSource(f)    // include flaeg
+		s.AddSource(toml) // include the TOML file
+	}
+
 	if _, err := s.LoadConfig(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
